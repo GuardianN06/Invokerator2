e914678384e6125850caa45c8174f881dbb022db
GuardianN06 <104389989+GuardianN06@users.noreply.github.com>
2024-01-14 02:13:34 +0100

    Add files via upload

---
+++ Invokerator2/Invokerator2/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Invokerator2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.AllowDrop = true;
+            this.Start.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Start.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Start.Location = new System.Drawing.Point(12, 12);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(303, 90);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Click to run a program as invoker / Drag and Drop";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(327, 114);
+            this.Controls.Add(this.Start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Invokerator2";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+    }
+}
+
---
+++ Invokerator2/Invokerator2/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Invokerator2
+{
+    public partial class Form1 : Form
+    {
+        private bool isCmdModified = false;
+        private string tempCmdDirectory;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += Form1_DragEnter;
+            this.DragDrop += Form1_DragDrop; 
+            this.Start.AllowDrop = true;
+            this.Start.DragEnter += Form1_DragEnter;
+            this.Start.DragDrop += Form1_DragDrop;
+            this.FormClosing += Form1_FormClosing;
+        }
+
+        private async void Start_Click(object sender, EventArgs e)
+        {
+            if (!isCmdModified)
+            {
+                ModifyCmd();
+                isCmdModified = true;
+            }
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string command = $"{GetModifiedCmdPath()} /c \"cd /d \"{Directory.GetCurrentDirectory()}\" & set __COMPAT_LAYER=RUNASINVOKER & start \"\" \"{openFileDialog.FileName}\" && exit\"";
+
+                await RunCommandAsync(command);
+            }
+        }
+
+        private void ModifyCmd()
+        {
+            string windir = Environment.GetEnvironmentVariable("windir");
+            string cmdPath = Path.Combine(windir, "System32\\cmd.exe");
+
+            tempCmdDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(tempCmdDirectory);
+
+            string copyPath = Path.Combine(tempCmdDirectory, "cmd.exe");
+            File.Copy(cmdPath, copyPath, true);
+
+            byte[] bytes = File.ReadAllBytes(copyPath);
+            for (int i = 0; i < bytes.Length - 2; i++)
+            {
+                if (bytes[i] == 0x44 && bytes[i + 1] == 0x4F && bytes[i + 2] == 0x53)
+                {
+                    bytes[i + 2] = 0x73;
+                    break;
+                }
+            }
+            File.WriteAllBytes(copyPath, bytes);
+        }
+
+        private string GetModifiedCmdPath()
+        {
+            return Path.Combine(tempCmdDirectory, "cmd.exe");
+        }
+
+        private Task<int> RunCommandAsync(string arguments)
+        {
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName = GetModifiedCmdPath(),
+                Arguments = arguments,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+            };
+
+            var process = new Process { StartInfo = processStartInfo };
+            var tcs = new TaskCompletionSource<int>();
+
+            process.EnableRaisingEvents = true;
+            process.Exited += (sender, args) =>
+            {
+                tcs.SetResult(process.ExitCode);
+                process.Dispose();
+            };
+
+            process.Start();
+
+            return tcs.Task;
+        }
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length > 0)
+            {
+                string filePath = files[0];
+
+                string command = $" /c \"cd /d \"{Directory.GetCurrentDirectory()}\" & set __COMPAT_LAYER=RUNASINVOKER & start \"\" \"{filePath}\" && exit\"";
+
+                RunCommandAsync(command);
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (isCmdModified == true)
+            {
+                string modifiedCmdPath = GetModifiedCmdPath();
+                if (File.Exists(modifiedCmdPath))
+                {
+                    try
+                    {
+                        File.Delete(modifiedCmdPath);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error deleting modified cmd.exe: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    Application.Exit();
+                }
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
